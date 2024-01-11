6bc2da94844a0821cd9893dbed82b4d32fb35a96
Sphesihle-Duma <dumasphesihle22@gmail.com>
2024-01-11 13:03:54 -0800

    Basic annotations-to string

---
+++ 0x00-python_variable_annotations/3-to_str.py
@@ -0,0 +1,9 @@
+#!/usr/bin/env python3
+"""type-annotated function module"""
+
+
+def to_str(n: float) -> str:
+    """type-annotated function that convert
+       a float to a string
+    """
+    return str(n)
