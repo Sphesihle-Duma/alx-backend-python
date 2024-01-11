9e22077671b7977359dbc0cbd5c0e08192bb3862
Sphesihle-Duma <dumasphesihle22@gmail.com>
2024-01-11 13:55:27 -0800

    import list from typing

--- 0x00-python_variable_annotations/5-sum_list.py
+++ 0x00-python_variable_annotations/5-sum_list.py
@@ -1,8 +1,8 @@
 #!/usr/bin/python3
 """type-annotated function module"""
+from typing import List
 
-
-def sum_list(input_list: [float]) -> float:
+def sum_list(input_list: List[float]) -> float:
     """Calculating sum of float"""
     sum = 0
     for input in input_list:
