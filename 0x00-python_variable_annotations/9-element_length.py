3298680e0925c58adc6cc4ce77f79561c11194ce
Sphesihle-Duma <dumasphesihle22@gmail.com>
2024-01-12 06:28:37 -0800

    Convert a normal function to type-annotated one

---
+++ 0x00-python_variable_annotations/9-element_length.py
@@ -0,0 +1,8 @@
+#!/usr/bin/env python3
+"""type-annotated functions module"""
+from typing import List, Tuple
+
+
+def element_length(lst: List[str]) -> List[Tuple[str, int]]:
+    """Returning a list of Tuples"""
+    return [(i, len(i)) for i in lst]
