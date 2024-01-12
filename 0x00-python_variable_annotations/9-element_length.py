780f4f786548630b841e144f9f9b253f65e19897
Sphesihle-Duma <dumasphesihle22@gmail.com>
2024-01-12 09:41:21 -0800

    giving i type variable

--- 0x00-python_variable_annotations/9-element_length.py
+++ 0x00-python_variable_annotations/9-element_length.py
@@ -5,4 +5,4 @@ from typing import List, Tuple
 
 def element_length(lst: List[str]) -> List[Tuple[str, int]]:
     """Returning a list of Tuples"""
-    return [(i, len(i)) for i in lst]
+    return [(i, len(i)) for i:str in lst]
