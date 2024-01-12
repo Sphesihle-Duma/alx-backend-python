e1eb13b6092b6eebe242b66289061e9f5bd14b13
Sphesihle-Duma <dumasphesihle22@gmail.com>
2024-01-12 14:38:49 -0800

    fixing the code

--- 0x00-python_variable_annotations/101-safely_get_value.py
+++ 0x00-python_variable_annotations/101-safely_get_value.py
@@ -1,18 +1,14 @@
 #!/usr/bin/env python3
-"""type-annotated function module"""
-from typing import TypeVar, Mapping, Any, Union
+""" Module documentation """
+from typing import Mapping, Any, Union, TypeVar
 
-T = TypeVar('T')
+T = TypeVar("T")
 
 
 def safely_get_value(
-    dct: Mapping[Any, T],
-    key: Any,
-    default: Union[T, None]
+    dct: Mapping, key: Any, default: Union[T, None] = None
 ) -> Union[Any, T]:
-    """Checks if the key is in the dct; if so, return the value for the key,
-    else return None.
-    """
+    """Func doc"""
     if key in dct:
         return dct[key]
     else:
