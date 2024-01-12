8c00d89cbf943eda09bfb15bc1964c105f01d3a6
Sphesihle-Duma <dumasphesihle22@gmail.com>
2024-01-12 10:18:23 -0800

    Returning the list of tuples that has a sequence

--- 0x00-python_variable_annotations/9-element_length.py
+++ 0x00-python_variable_annotations/9-element_length.py
@@ -3,6 +3,6 @@
 from typing import List, Tuple, Sequence, Iterable
 
 
-def element_length(lst: Iterable[Sequence]) -> List[Tuple[str, int]]:
+def element_length(lst: Iterable[Sequence]) -> List[Tuple[Sequence, int]]:
     """Returning a list of Tuples"""
     return [(i, len(i)) for i in lst]
