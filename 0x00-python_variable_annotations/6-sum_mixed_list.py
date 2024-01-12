641011f2faca67e073b27ebe51f67735229442b3
Sphesihle-Duma <dumasphesihle22@gmail.com>
2024-01-12 05:02:57 -0800

    Making mixed list using union

--- 0x00-python_variable_annotations/6-sum_mixed_list.py
+++ 0x00-python_variable_annotations/6-sum_mixed_list.py
@@ -1,9 +1,9 @@
 #!/usr/bin/env python3
 """type-annotated function module"""
-from typing import List
+from typing import List, Union
 
 
-def sum_mixed_list(mxd_lst: List[int or float]) -> float:
+def sum_mixed_list(mxd_lst: List[Union[int, float]]) -> float:
     """Calculating the sum of the mixed element in a list"""
     sum: float = 0
     for input in mxd_lst:
