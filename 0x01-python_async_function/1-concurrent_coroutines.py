1ab97fc770792659c69a7a4b9f2bf48932e7d5c1
Sphesihle-Duma <dumasphesihle22@gmail.com>
2024-01-16 03:58:12 -0800

    Implementing concurrent logic

---
+++ 0x01-python_async_function/1-concurrent_coroutines.py
@@ -0,0 +1,13 @@
+#!/usr/bin/env python3
+"""Concurent functions module"""
+import asyncio
+from typing import List
+wait_random = __import__('0-basic_async_syntax').wait_random
+
+
+async def wait_n(n: int, max_delay: int) -> List[float]:
+    """Running async functions concurently"""
+    tasks = [wait_random(max_delay) for i in range(0, n)]
+    result: List[float] = await asyncio.gather(*tasks)
+    result.sort()
+    return result
