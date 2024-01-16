bc23b160ab138e902215b30ca0868b1de77dadbe
Sphesihle-Duma <dumasphesihle22@gmail.com>
2024-01-16 04:48:46 -0800

    Using sorted instead of sort

--- 0x01-python_async_function/1-concurrent_coroutines.py
+++ 0x01-python_async_function/1-concurrent_coroutines.py
@@ -1,13 +1,12 @@
 #!/usr/bin/env python3
 """Concurent functions module"""
 import asyncio
-from typing import List, Any
+from typing import List
 wait_random = __import__('0-basic_async_syntax').wait_random
 
 
 async def wait_n(n: int, max_delay: int) -> List[float]:
     """Running async functions concurently"""
-    tasks: List[any] = [wait_random(max_delay) for i in range(0, n)]
+    tasks = [wait_random(max_delay) for i in range(0, n)]
     result: List[float] = await asyncio.gather(*tasks)
-    result.sort()
-    return result
+    return sorted(result)
