e69bbc54af46babbf9b7824222eeaf5cc3c3d4a5
Sphesihle-Duma <dumasphesihle22@gmail.com>
2024-01-17 10:45:52 -0800

    Removing genarator type hint

--- 0x02-python_async_comprehension/1-async_comprehension.py
+++ 0x02-python_async_comprehension/1-async_comprehension.py
@@ -1,10 +1,10 @@
 #!/usr/bin/env python3
 """Implementing the generator function"""
 import asyncio
-from typing import List, Generator
+from typing import List
 async_generator = __import__('0-async_generator').async_generator
 
 
-async def async_comprehension() -> Generator[List[float], None, None]:
+async def async_comprehension() -> List[float]:
     """Returning the list of floats"""
     return [i async for i in async_generator()]
